8773e25e0bdd0b0bb0e84d55f0d4478a25504ca3
Daniel <danielgibson.pe@gmail.com>
2025-09-04 10:55:16 +0200

    Dashboard and Manage Users added

--- Rectify/Controllers/AdminController.cs
+++ Rectify/Controllers/AdminController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Rectify.Controllers
 {
     public class AdminController : Controller
     {
+        [Authorize]
         public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult ManageUsers()
         {
             return View();
         }
